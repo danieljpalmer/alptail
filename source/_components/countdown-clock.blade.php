eb64848a6dac714c309aa933f2c50636ca74deef
Daniel Palmer <danp1995mail@gmail.com>
2020-06-04 18:50:25 +1000

    Starting work on countdown clock

--- source/_components/countdown-clock.blade.php
+++ source/_components/countdown-clock.blade.php
@@ -1,7 +1,32 @@
-<div class="flex items-center justify-between space-x-4">
+<div class="flex items-center space-x-6">
 
+    <div class="flex flex-col items-center justify-between space-y-1">
 
-    <h2>Test</h2>
+        <span class="text-4xl font-bold text-primary w-20 h-20 flex items-center justify-center border border-gray-200 rounded-sm">24</span>
+        <span class="text-sm text-gray-500">Days</span>
+
+    </div>
+
+    <div class="flex flex-col items-center justify-between space-y-1">
+
+        <span class="text-4xl font-bold text-primary w-20 h-20 flex items-center justify-center border border-gray-200 rounded-sm">24</span>
+        <span class="text-sm text-gray-500">Hours</span>
+
+    </div>
+
+    <div class="flex flex-col items-center justify-between space-y-1">
+
+        <span class="text-4xl font-bold text-primary w-20 h-20 flex items-center justify-center border border-gray-200 rounded-sm">24</span>
+        <span class="text-sm text-gray-500">Minutes</span>
+
+    </div>
+
+    <div class="flex flex-col items-center justify-between space-y-1">
+
+        <span class="text-4xl font-bold text-primary w-20 h-20 flex items-center justify-center border border-gray-200 rounded-sm">24</span>
+        <span class="text-sm text-gray-500">Seconds</span>
+
+    </div>
 
 
 </div>
