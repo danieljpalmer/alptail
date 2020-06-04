3ae7c52af7d65aa549924577b29fca190dbc7bf6
Daniel Palmer <danp1995mail@gmail.com>
2020-06-04 14:15:49 +1000

    Tweaking avatar with dropdown component

--- source/_components/avatar-dropdown-menu.blade.php
+++ source/_components/avatar-dropdown-menu.blade.php
@@ -6,24 +6,24 @@
 
 class="relative">
 
-    <button @click="open = true" class="w-12 h-12 rounded-full flex items-center justify-center overflow-hidden relative cursor-pointer hover:shadow-md focus:outline-none focus:shadow-outline transition duration-150">
+    <button @click="open = true" class="w-12 h-12 rounded-full flex items-center justify-center overflow-hidden relative cursor-pointer hover:shadow-md focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
         <img src="https://randomuser.me/api/portraits/women/42.jpg" class="absolute" alt="Your Profile Image">
     </button>
 
-    <div x-show.transition="open" @click.away="open = false" class="absolute mt-2 p-4 bg-white shadow-lg">
+    <div x-show.transition="open" @click.away="open = false" class="absolute mt-2 bg-white shadow-lg rounded-sm overflow-hidden">
         <nav>
-            <ul class="text-sm text-gray-600">
-                <li>
-                    <a class="transition duration-150 hover:text-indigo-600 hover:underline focus:underline" href="" target="_blank" title="Link 1">Link 1</a>
+            <ul class="text-sm text-gray-600 py-2">
+                <li class="flex">
+                    <a class="px-6 py-2 transition duration-150 hover:text-indigo-600 hover:underline hover:bg-gray-100 focus:underline" href="" target="_blank" title="Link 1">Link 1</a>
                 </li>
-                <li>
-                    <a class="transition duration-150 hover:text-indigo-600 hover:underline focus:underline" href="" target="_blank" title="Link 2">Link 2</a>
+                <li class="flex">
+                    <a class="px-6 py-2 transition duration-150 hover:text-indigo-600 hover:underline hover:bg-gray-100 focus:underline" href="" target="_blank" title="Link 2">Link 2</a>
                 </li>
-                <li>
-                    <a class="transition duration-150 hover:text-indigo-600 hover:underline focus:underline" href="" target="_blank" title="Link 3">Link 3</a>
+                <li class="flex">
+                    <a class="px-6 py-2 transition duration-150 hover:text-indigo-600 hover:underline hover:bg-gray-100 focus:underline" href="" target="_blank" title="Link 3">Link 3</a>
                 </li>
-                <li>
-                    <a class="transition duration-150 hover:text-indigo-600 hover:underline focus:underline" href="" target="_blank" title="Link 4">Link 4</a>
+                <li class="flex">
+                    <a class="px-6 py-2 transition duration-150 hover:text-indigo-600 hover:underline hover:bg-gray-100 focus:underline" href="" target="_blank" title="Link 4">Link 4</a>
                 </li>
             </ul>
         </nav>
