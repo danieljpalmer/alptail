075c19175c0ae34643da279b9da2642557d4c2c8
Daniel Palmer <danp1995mail@gmail.com>
2020-06-08 17:32:08 +1000

    Arrows on slider

--- source/_components/slider.blade.php
+++ source/_components/slider.blade.php
@@ -36,8 +36,17 @@ class="max-w-screen-sm"
 
 
         <div class="w-full flex items-center justify-end space-x-4 text-indigo-600 text-xs mt-4">
-            <button class="uppercase hover:underline focus:underline focus:outline-none" :class="{ 'text-gray-500' : selected === 0 }" @click="selected > 0 ? selected-- : null">Previous</button>
-            <button class="uppercase hover:underline focus:underline focus:outline-none" :class="{ 'text-gray-500' : selected === (images.length - 1) }" @click="selected < (images.length - 1) ? selected ++ : null">Next</button>
+            <button class="uppercase hover:underline focus:underline focus:outline-none" :class="{ 'text-gray-500' : selected === 0 }" @click="selected > 0 ? selected-- : null">
+
+
+                <svg class="w-4 fill-current transform rotate-180" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 492.004 492.004"><path d="M382.678 226.804L163.73 7.86C158.666 2.792 151.906 0 144.698 0s-13.968 2.792-19.032 7.86l-16.124 16.12c-10.492 10.504-10.492 27.576 0 38.064L293.398 245.9l-184.06 184.06c-5.064 5.068-7.86 11.824-7.86 19.028 0 7.212 2.796 13.968 7.86 19.04l16.124 16.116c5.068 5.068 11.824 7.86 19.032 7.86s13.968-2.792 19.032-7.86L382.678 265c5.076-5.084 7.864-11.872 7.848-19.088.016-7.244-2.772-14.028-7.848-19.108z"/></svg>
+
+            </button>
+            <button class="uppercase hover:underline focus:underline focus:outline-none" :class="{ 'text-gray-500' : selected === (images.length - 1) }" @click="selected < (images.length - 1) ? selected ++ : null">
+
+                <svg class="w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 492.004 492.004"><path d="M382.678 226.804L163.73 7.86C158.666 2.792 151.906 0 144.698 0s-13.968 2.792-19.032 7.86l-16.124 16.12c-10.492 10.504-10.492 27.576 0 38.064L293.398 245.9l-184.06 184.06c-5.064 5.068-7.86 11.824-7.86 19.028 0 7.212 2.796 13.968 7.86 19.04l16.124 16.116c5.068 5.068 11.824 7.86 19.032 7.86s13.968-2.792 19.032-7.86L382.678 265c5.076-5.084 7.864-11.872 7.848-19.088.016-7.244-2.772-14.028-7.848-19.108z"/></svg>
+
+            </button>
         </div>
 
 </div>
